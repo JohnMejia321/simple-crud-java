010119eec155f22937a1ce133eb7cc71fef51180
JohnMejia321 <john.mejias@udea.edu.co>
2022-10-07 20:46:07 -0500

    first commit

---
+++ src/peliculas/Main.java
@@ -0,0 +1,12 @@
+package peliculas;
+import javax.swing.JOptionPane;
+import peliculas.ui.UiPeliculas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    UiPeliculas.interfazUsuario();
+
+    }
+}
